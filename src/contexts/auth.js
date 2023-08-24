5bfd942c39c2432ba97ff481ddb786c1627c5368
Jamesson Faria <jamessonjr@gmail.com>
2023-08-24 15:12:32 -0300

    added private routes

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -3,6 +3,7 @@ import { auth, db } from "../services/firebaseConnection";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -14,8 +15,24 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadUser() {
+      const storageUser = localStorage.getItem("@ticketsPRO");
+
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+
+      setLoading(false);
+    }
+
+    loadUser();
+  }, []);
 
   async function signIn(email, password) {
     setLoadingAuth(true);
@@ -83,6 +100,12 @@ function AuthProvider({ children }) {
     localStorage.setItem("@ticketsPRO", JSON.stringify(data));
   }
 
+  async function logout() {
+    await signOut(auth);
+    localStorage.removeItem("@ticketsPRO");
+    setUser(null);
+  }
+
   return (
     <AuthContext.Provider
       value={{
@@ -90,7 +113,9 @@ function AuthProvider({ children }) {
         user,
         signIn,
         signUp,
+        logout,
         loadingAuth,
+        loading,
       }}
     >
       {children}
