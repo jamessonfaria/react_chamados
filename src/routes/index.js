a886ea02870694fc977c674a1a6b58804e5e7601
Jamesson Faria <jamessonjr@gmail.com>
2023-08-24 11:54:38 -0300

    created screen signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
 
-function RoutesApp(){
-  return(
+function RoutesApp() {
+  return (
     <Routes>
-      <Route path="/" element={ <SignIn/> } />
-      <Route path="/register" element={ <SignUp/> } />
+      <Route path="/" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  )
+  );
 }
 
 export default RoutesApp;
